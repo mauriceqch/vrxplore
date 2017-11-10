fc7cd70cc0feac47b7bf3a06b5df1811889815a1
Maurice Quach <maurice.quach@gmail.com>
2017-11-10 12:07:13 +0100

    Initial Vive version

---
+++ Assets/MainScene/Vive/ViveSyncHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class ViveSyncHelper {
+    public static void syncPosition(GameObject o, VRNode node)
+    {
+        o.transform.position = InputTracking.GetLocalPosition(node);
+    }
+}
