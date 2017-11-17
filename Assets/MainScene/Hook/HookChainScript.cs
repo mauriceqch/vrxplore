bf2bc09ffa94a7c98fd6762c6e294f06f1f6fd90
Maurice Quach <maurice.quach@gmail.com>
2017-11-17 01:02:31 +0100

    Add hook attraction

--- Assets/MainScene/Hook/HookChainScript.cs
+++ Assets/MainScene/Hook/HookChainScript.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 namespace Hook
 {
     public class HookChainScript : MonoBehaviour {
-        public float chainSpeed;
+        public float chainSpeed = 1000;
+        public float attractionForce = 1000;
+        public float hookPadding = 1;
+
         public Vector3 destination;
+        public GameObject attractedObject;
 
         // Use this for initialization
         void Start () {
@@ -15,9 +19,18 @@ namespace Hook
         
         // Update is called once per frame
         void Update () {
-            float step = chainSpeed * Time.deltaTime;
+            float deltaTime = Time.smoothDeltaTime;
 
-            transform.position = Vector3.MoveTowards(transform.position, destination, step);
+            transform.position = Vector3.MoveTowards(transform.position, destination, chainSpeed * deltaTime);
+        }
+
+        void FixedUpdate()
+        {
+            Transform playerTransform = attractedObject.transform;
+            if (Vector3.Distance(playerTransform.position, transform.position) > hookPadding)
+            {
+                attractedObject.GetComponent<Rigidbody>().AddForce((destination - playerTransform.position).normalized * attractionForce);
+            }
         }
     }
 }
--- Assets/MainScene/Hook/HookScript.cs
+++ Assets/MainScene/Hook/HookScript.cs
@@ -5,7 +5,9 @@ using Hook;
 
 namespace Hook
 {
-    public class HookScript : MonoBehaviour {
+    public class HookScript : MonoBehaviour
+    {
+        public GameObject attractedObject;
         public GameObject chain;
         public GameObject dotPointer;
         public GameObject hookChainSpawnPoint;
@@ -14,15 +16,16 @@ namespace Hook
         private GameObject currentChain;
 
         // Use this for initialization
-        void Start () {
+        void Start()
+        {
             currentDotPointer = Instantiate(dotPointer);
         }
-        
+
         // Update is called once per frame
-        void Update () {
+        void Update()
+        {
             Transform spawnPointTransform = hookChainSpawnPoint.transform;
             Vector3 direction = transform.TransformDirection(Vector3.forward);
-            Debug.DrawRay(spawnPointTransform.position, direction * 1000, Color.red);
 
             RaycastHit hit;
             if (Physics.Raycast(spawnPointTransform.position, direction, out hit))
@@ -32,23 +35,30 @@ namespace Hook
 
                 if (Input.GetMouseButtonDown(0))
                 {
-                        print("Raycast hit, distance : " + hit.distance + " pos : " + hit.point);
+                    print("Raycast hit, distance : " + hit.distance + " pos : " + hit.point);
 
-                        if (currentChain != null)
-                        {
-                            Destroy(currentChain);
-                        }
+                    if (currentChain != null)
+                    {
+                        Destroy(currentChain);
+                    }
 
-                        Debug.Log("Instantiating chain");
-
-                        currentChain = Instantiate(chain, spawnPointTransform.position, spawnPointTransform.rotation);
-
-                        currentChain.GetComponent<HookChainScript>().destination = hit.point;
+                    createChain(hit);
                 }
-            } else
+            }
+            else
             {
                 currentDotPointer.SetActive(false);
             }
+        }
+
+        void createChain(RaycastHit hit)
+        {
+            Debug.Log("Instantiating chain");
+            Transform spawnPointTransform = hookChainSpawnPoint.transform;
+            currentChain = Instantiate(chain, spawnPointTransform.position, spawnPointTransform.rotation);
+            HookChainScript hookChainScript = currentChain.GetComponent<HookChainScript>();
+            hookChainScript.destination = hit.point;
+            hookChainScript.attractedObject = attractedObject;
         }
     }
 }
