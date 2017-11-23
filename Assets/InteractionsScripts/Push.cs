5fc1f144337ff37f0d0e240e1fab4cfd388b365e
fboucaud <fabien@boucaud.net>
2017-11-23 09:59:20 +0100

    Push script, needs tweaking

---
+++ Assets/InteractionsScripts/Push.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour {
+
+    public const int RAYCASTLENGTH = 20;
+    public const float pushForce = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hitInfo;
+        Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
+        bool rayCasted = Physics.Raycast(ray, out hitInfo, RAYCASTLENGTH);
+
+        if (rayCasted)
+        {
+            rayCasted = hitInfo.transform.CompareTag("Pushable");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (rayCasted)
+            {
+                Debug.Log("Object pushed");
+                hitInfo.rigidbody.AddRelativeForce((hitInfo.rigidbody.position + ray.direction * RAYCASTLENGTH) * pushForce);
+            }
+        }
+    }
+}
