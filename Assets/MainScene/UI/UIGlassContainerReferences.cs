9691bb228b5d930d7d0a22fffc809ea9c0a5f14f
Maurice Quach <maurice.quach@gmail.com>
2017-12-08 12:24:05 +0100

    Add UIGlass

---
+++ Assets/MainScene/UI/UIGlassContainerReferences.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGlassContainerReferences : MonoBehaviour {
+    public GameObject UIGlass;
+    public GameObject Canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MainScene/UIGlassContainerSync.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+class UIGlassContainerSync
+{
+    static UIGlassContainerSync()
+    {
+        EditorApplication.update += Update;
+    }
+
+    static void Update()
+    {
+        Debug.Log("Updating");
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("UIGlassContainer");
+        foreach (GameObject obj in objects)
+        {
+            UIGlassContainerReferences refs = obj.GetComponent<UIGlassContainerReferences>();
+            GameObject UIGlass = refs.UIGlass;
+            GameObject Canvas = refs.Canvas;
+
+            Vector3 UIGlassScale = UIGlass.transform.localScale;
+            Vector3 CanvasPosition = Canvas.transform.localPosition;
+            Canvas.transform.localPosition = new Vector3(
+                CanvasPosition.x,
+                - (1 - UIGlassScale.y) / 2.0f,
+                (1 - UIGlassScale.z) / 2.0f
+            );
+        }
+    }
+}
